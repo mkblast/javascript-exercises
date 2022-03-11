a58cecfc88f6f2c00e2407add71d6ad48e105a7f
Karim Mk <mk47blast@protonmail.com>
2022-03-11 17:26:46 +0100

    exo 05 done baby!!!!!!!!!!!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let resault = 0;
+  let bigNum;
+  let smallNum;
+
+  if (num1 >= 0 && typeof(num1) == 'number'  && typeof(num2) == 'number') {
+    if (num1 > num2){
+      bigNum = num1;
+      smallNum = num2;
+
+    } else {
+      bigNum = num2;
+      smallNum = num1;
+    }
+
+    for (; smallNum <= bigNum; smallNum++) {
+      resault += smallNum;
+    }
+
+    return resault;
+
+  } else {
+    return 'ERROR'
+  }
+
+
 
 };
 
